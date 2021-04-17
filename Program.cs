58416093fa883b34dc8af3020e3a139a992ff995
Karl Frick <karl.frick5@gmail.com>
2021-04-16 22:20:14 -0400

    Added comments for better understand of shuffle

--- Program.cs
+++ Program.cs
@@ -23,17 +23,17 @@ namespace BlackJackCS
             //create blank list, or deck to hold the cards with their 2 values
             var deck = new List<string>();
 
-            //need to shuffle cards
             //create 2 lists to hold a card's 2 values
             var suits = new List<string>() { "Clubs", "Hearts", "Diamonds", "Spades" };
             var ranks = new List<string>() { "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "Jack", "Queen", "King" };
 
-            foreach (var rank in ranks)
+            // need to create loop to assign each suit with rank
+            foreach (var rank in ranks) // go through each rank
             {
-                foreach (var suit in suits)
+                foreach (var suit in suits) // go through each suit for each rank
                 {
-                    var card = $"{rank} of {suit}";
-                    deck.Add(card);
+                    var card = $"{rank} of {suit}"; // for each rank/suit combo add to a card
+                    deck.Add(card); // add each card to the list deck
                 }
             }
 
@@ -57,24 +57,42 @@ namespace BlackJackCS
 
             // need to deal 2 cards to player and display
             var playerCards = $"{deck[0]} and {deck[1]}";
+            // remove those cards from the deck
+            deck.RemoveAt(0);
+            deck.RemoveAt(1);
             Console.WriteLine($"You were dealt a {playerCards}");
 
 
+
             // need to deal 2 cards to the house, but will NOT be displayed
-            var houseCards = $"{deck[2]} and {deck[3]}";
+            var houseCards = $"{deck[0]} and {deck[1]}";
+            // remove those cards from the deck
+            deck.RemoveAt(0);
+            deck.RemoveAt(1);
+
+            // need to create if statement to determine is hand is > 21
 
 
             // need to ask the player if they want to hit or stand
             Console.WriteLine("Do you want to HIT?");
             var hitAnswer = Console.ReadLine();
-            if (hitAnswer == "y" || hitAnswer == "Y" || hitAnswer == "Hit" || hitAnswer == "hit" || hitAnswer == "HIT")
+            if (hitAnswer == "y" || hitAnswer == "Y" || hitAnswer == "YES" || hitAnswer == "yes" || hitAnswer == "Yes")
             {
                 Console.WriteLine("----DEALING ANOTHER CARD----");
             }
 
             // if player hit we need to add next card from deck to their hand
-            playerHand.Add($"{playerCards} and {deck[0]}");
-            Console.WriteLine($"{playerHand}");
+            var playerFirstHand = ($"{playerCards} and {deck[0]}");
+            playerHand.Add($"{deck[0]}");
+            // need to remove that card from the deck
+            deck.RemoveAt(0);
+            Console.WriteLine($"{playerFirstHand}");
+
+
+
+            // need to determine if house will hit or stand
+            // is the houses hand equal to or more than 17?
+            // is the houses hand equal to or less than 16?
 
 
 
